d646e3149698abe8a7f696c91e51cac83ab8ab81
Z <zyd@ZdeMacBook-Pro.lan>
2022-07-12 01:10:18 +0800

    feat: add something about article;

--- apps/server/src/module/article/article.controller.ts
+++ apps/server/src/module/article/article.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { CreateArticleDto } from './dto/article.dto';
 import { ArticleService } from './article.service';
 
 @Controller('article')
@@ -6,12 +15,28 @@ export class ArticleController {
   constructor(private readonly articleService: ArticleService) {}
 
   @Get()
-  getHello(): string {
+  findAll(): string {
     return this.articleService.getHello();
   }
 
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns a #${id} article`;
+  }
+
   @Post()
-  addArticle(): string {
+  create(@Body() createArticleDto: CreateArticleDto): string {
+    console.log(createArticleDto);
     return 'add article success';
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string) {
+    return `This action updates a #${id} article`;
+  }
+
+  @Delete(':id')
+  delete(@Param(':id') id: string) {
+    return `This action removes a #${id} article`;
   }
 }
